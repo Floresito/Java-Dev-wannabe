89e09ed9f13145ce8a20dc9afd7fd537808fb5b9
Diego Flores <floresdside@gmail.com>
2025-11-10 15:23:56 -0600

    Practica 1: " Introducción a Java y Configuración del En torno" añadida
    Practica 1 añadida al repo

---
+++ Practica1DFL/HolaMundoDFL/src/holamundodfl/HolaMundoDFL.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundodfl;
+
+/**
+ *
+ * @author a
+ */
+public class HolaMundoDFL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Diego Flores López");
+        System.out.println("Matrícula: 2066033");
+        System.out.println(java.time.LocalDate.now());
+    }
+    
+}
